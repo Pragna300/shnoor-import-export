519e36ae3815d851653911970c2cec8ba63137d4
khaleel-shnoor <khaleel@shnoor.com>
2026-04-15 19:36:45 +0530

    Initial setup completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="w-full flex justify-center items-center h-[100vh]">
+
+        <h1 className="text-7xl" >AI Import Export System</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
